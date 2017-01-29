35b2fb71afe9787efecaa62a590e493f8329dcb5
Robert Smiles Jr <smilesr120@gmail.com>
2017-01-29 10:03:29 -0500

    added call to youtube api

--- public/scripts.js
+++ public/scripts.js
@@ -4,24 +4,47 @@ $( document ).ready(function() {
 
         e.preventDefault();
         var input = $(".artist_info").text();
-        $.ajax({
-          type: "GET",
-          data: input,
-          url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&callback=?",
-          contentType: "application/json; charset=utf-8",
-          async: false,
-          dataType: "json",
-          success: function (data, textStatus, jqXHR) {
-            console.log(data);
-            showResults(data);
-          },
-          error: function (errorMessage) {
+        goToYouTube();
+
+        function showResponse(response) {
+          var responseString = JSON.stringify(response, '', 2);
+          document.querySelector('.showIt').innerHTML += responseString;
+          console.log("showresponse");
         }
-      }
-    )});
 
-    function showResults(data){
-      $( ".results" ).append( data );
+        function goToYouTube() {
+            gapi.client.load('youtube', 'v3', function(){
+                gapi.client.setApiKey();
+                search();
+            })
+        }
 
-    }
-  })
+        function search() {
+            var request = gapi.client.youtube.search.list({
+                part: 'snippet',
+                q: input
+            });
+            request.execute(showResponse);
+        }
+
+    })
+});
+
+
+
+    //     $.ajax({
+    //       type: "GET",
+    //       data: input,
+    //       url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&callback=?",
+    //       contentType: "application/json; charset=utf-8",
+    //       async: false,
+    //       dataType: "json",
+    //       success: function (data, textStatus, jqXHR) {
+    //         console.log(data);
+    //         showResults(data);
+    //       },
+    //       error: function (errorMessage) {
+    //       }
+    //     })
+    // });
+
