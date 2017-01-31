ff4c52db94fa618ce7dc490ea07e5e4ebbaf0a59
Robert Smiles Jr <smilesr120@gmail.com>
2017-01-31 13:59:29 -0500

    reformatted view into columns; parsed wiki response to search for results that relate to music

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
     $('.results').on('click','.artist_info', function(e){
-        console.log("inside");
-
         e.preventDefault();
         var artistInfo = $(".artist_info").text();
         var songInfo = $(".song_info").text();
@@ -11,12 +9,11 @@ $( document ).ready(function() {
         function showResponse(response) {
           var videoId = response.items[0].id.videoId;
           $('#existing-iframe-example').attr( "src", "https://www.youtube.com/embed/" + videoId );
-          console.log("showresponse");
         }
 
         function goToYouTube() {
             gapi.client.load('youtube', 'v3', function(){
-                gapi.client.setApiKey('');
+                gapi.client.setApiKey('AIzaSyATNpPM04GxYChGc1wRD9TlCg3m4UG7-oc');
                 search();
             })
         }
@@ -67,58 +64,67 @@ $( document ).ready(function() {
   // })
 
 var tag = document.createElement('script');
-  tag.id = 'iframe-demo';
-  tag.src = 'https://www.youtube.com/iframe_api';
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('existing-iframe-example', {
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('existing-iframe-example', {
         events: {
           'onReady': onPlayerReady,
           'onStateChange': onPlayerStateChange
         }
-    });
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
   }
-  function onPlayerReady(event) {
-           event.target.playVideo();
-  }
-  function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-      setTimeout(stopVideo, 6000);
-      done = true;
-    }
-  }
-  function stopVideo() {
-    player.stopVideo();
-  }
-//*********************//
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
 $( document ).ready(function() {
-    $('.results').on('click','.artist_info', function(e){
-        console.log("inside");
-
-        e.preventDefault();
-        var input = $(".artist_info").text();
-        $.ajax({
-          type: "GET",
-          data: input,
-          url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&callback=?",
-          contentType: "application/json; charset=utf-8",
-          async: false,
-          dataType: "json",
-          success: function (data, textStatus, jqXHR) {
-            console.log(data);
-            showResults(data);
-          },
-          error: function (errorMessage) {
-        }
+  $('.results').on('click','.artist_info', function(e){
+      e.preventDefault();
+      var input = $(".artist_info").text();
+      $.ajax({
+        type: "GET",
+        data: input,
+        url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&callback=?",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+          console.log(data);
+          findMusician(data);
+          // showResults(data[2][0]);
+        },
+        error: function (errorMessage) {
       }
-    )});
-
-    function showResults(data){
-      $( ".results" ).append( data );
-
     }
-  })
+  )});
+
+  function findMusician(data){
+    var hits = data[2];
+    for (var i=0; i<hits.length; i++){
+      if (hits[i].search(/(band|singer|musician)/) != -1){
+        showResults(hits[i]);
+      }
+    }
+  }
+  function showResults(data){
+    $( ".results" ).append( data );
+  }
+})
 
