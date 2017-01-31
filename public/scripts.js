b4a08cfd5f58038c9d9fd786829fd17cacf2c3f1
Robert Smiles Jr <smilesr120@gmail.com>
2017-01-31 10:41:37 -0500

    reinstated wiki lookup; began changing layout

--- public/scripts.js
+++ public/scripts.js
@@ -93,5 +93,32 @@ var tag = document.createElement('script');
   function stopVideo() {
     player.stopVideo();
   }
+//*********************//
+$( document ).ready(function() {
+    $('.results').on('click','.artist_info', function(e){
+        console.log("inside");
 
+        e.preventDefault();
+        var input = $(".artist_info").text();
+        $.ajax({
+          type: "GET",
+          data: input,
+          url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&callback=?",
+          contentType: "application/json; charset=utf-8",
+          async: false,
+          dataType: "json",
+          success: function (data, textStatus, jqXHR) {
+            console.log(data);
+            showResults(data);
+          },
+          error: function (errorMessage) {
+        }
+      }
+    )});
+
+    function showResults(data){
+      $( ".results" ).append( data );
+
+    }
+  })
 
