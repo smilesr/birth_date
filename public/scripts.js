61ed04472bcfdb054e453e0d10af3422f4601c1e
Robert Smiles Jr <smilesr120@gmail.com>
2017-02-08 22:13:20 -0500

    added additional filter on wiki search to eliminate wiki search results that show multiple entries for same artist or that have multiple artist with same name - used song name to refine the latter

--- public/scripts.js
+++ public/scripts.js
@@ -72,12 +72,10 @@ $( document ).ready(function() {
         var reg2 = /\s|\./;
         var i = arr.length;
         while (i--){
-        // for (var i = 0; i<arr.length; i++){
           if (arr[i].search(reg2) === 0 || arr[i] == false){
             arr.splice(i,1);
             continue;
           }
-
           arr;
         }
         var newStr = "";
@@ -89,7 +87,6 @@ $( document ).ready(function() {
           } else {
             newStr += (arr[i] + " ");
           }
-
         }
         return newStr;
       }
@@ -111,147 +108,28 @@ $( document ).ready(function() {
   });
   function findMusician(data){
     var hits = data[2];
+    var musicians = [];
     for (var i=0; i<hits.length; i++){
-      if (hits[i].search(/(band|singer|musician|rapper)/) != -1){
-        showResults(hits[i]);
+      if (hits[i].search(/(band|singer|musician|rapper|album|group)/) != -1){
+        musicians.push(hits[i]);
       }
+    }
+    if (musicians.length === 1){
+      showResults(musicians[0]);
+    } else {
+      var songName = $('.song_info').text();
+      var likelySelection = musicians[0];
+      for (var j=0; j<musicians.length; j++){
+        var m = musicians[j].toLowerCase();
+        var n = songName.toLowerCase().trim();
+        if (m.includes(n)) {
+          likelySelection = musicians[j];
+        }
+      }
+      showResults(likelySelection);
     }
   }
   function showResults(data){
     $( ".results" ).append( data );
   }
 })
-
-//****************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $( document ).ready(function() {
-//     $('.results').on('click','.artist_info', function(e){
-//         e.preventDefault();
-//         var artistInfo = $(".artist_info").text();
-//         var songInfo = $(".song_info").text();
-//         var input = artistInfo.concat(songInfo);
-//         goToYouTube();
-
-//         function showResponse(response) {
-//           var videoId = response.items[0].id.videoId;
-//           $('#existing-iframe-example').attr( "src", "https://www.youtube.com/embed/" + videoId );
-//         }
-
-//         function goToYouTube() {
-//             gapi.client.load('youtube', 'v3', function(){
-//                 gapi.client.setApiKey('AIzaSyATNpPM04GxYChGc1wRD9TlCg3m4UG7-oc');
-//                 search();
-//             })
-//         }
-
-//         function search() {
-//             var request = gapi.client.youtube.search.list({
-//                 part: 'snippet',
-//                 q: input
-//             });
-//             request.execute(showResponse);
-//         }
-
-//     })
-//     });
-
-// $( document ).ready(function() {
-//   $('.results').on('click','.artist_info', function(e){
-//       e.preventDefault();
-//       var input = $(".artist_info").text();
-//       $.ajax({
-//         type: "GET",
-//         data: input,
-//         url: "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&callback=?",
-//         contentType: "application/json; charset=utf-8",
-//         async: false,
-//         dataType: "json",
-//         success: function (data, textStatus, jqXHR) {
-//           console.log(data);
-//           findMusician(data);
-//           // showResults(data[2][0]);
-//         },
-//         error: function (errorMessage) {
-//       }
-//     }
-//   )
-
-
-//   });
-//   function findMusician(data){
-//     var hits = data[2];
-//     for (var i=0; i<hits.length; i++){
-//       if (hits[i].search(/(band|singer|musician)/) != -1){
-//         showResults(hits[i]);
-//       }
-//     }
-//   }
-//   function showResults(data){
-//     $( ".results" ).append( data );
-//   }
-// })
-
-
-
-
-
-
-
-
-
-
-// var tag = document.createElement('script');
-// tag.id = 'iframe-demo';
-// tag.src = 'https://www.youtube.com/iframe_api';
-// var firstScriptTag = document.getElementsByTagName('script')[0];
-// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// var player;
-// function onYouTubeIframeAPIReady() {
-//   player = new YT.Player('existing-iframe-example', {
-//         events: {
-//           'onReady': onPlayerReady,
-//           'onStateChange': onPlayerStateChange
-//         }
-//   });
-// }
-
-// function onPlayerReady(event) {
-//   event.target.playVideo();
-// }
-
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
-
-// function stopVideo() {
-//   player.stopVideo();
-// }
-
